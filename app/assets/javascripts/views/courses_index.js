c9c91eff78ca00c6a4a12404cc0d5cf5c5bfda86
Jeff Fiddler <jeffnv@gmail.com>
2015-02-03 09:38:56 -0800

    add loading spinner

--- app/assets/javascripts/views/courses_index.js
+++ app/assets/javascripts/views/courses_index.js
@@ -3,12 +3,30 @@ EliteGolfApp.Views.CoursesIndex = Backbone.CompositeView.extend({
     subviewConstructor: function() {
         return EliteGolfApp.Views.CoursesIndexItem;
     },
+    modelAdded: function(model) {
+        if (this.spinnerVisible) {
+            this.removeSpinner();
+            this.spinnerVisible = false;
+        }
+    },
     subviewSelector: ".courses-list",
     render: function() {
         this.$el.html(this.template({
             course: this.model
         }));
+
+        if (this.subviews(this.subviewSelector).length == 0) {
+            this.addSpinner();
+        }
+
         this.attachSubviews();
         return this;
-    }
+    },
+    addSpinner: function() {
+        this.$el.append("<div class='loader'>loading...</div>");
+        this.spinnerVisible = true;
+    },
+    removeSpinner: function() {
+        this.$('.loader').remove();
+    },
 });
